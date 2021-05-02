ba94cc9d4fdb084b5a376020681f771a3a6eed01
Ben Pitts <ben@metricfire.com>
2021-05-02 11:28:52 -0600

    link properties in header

--- src/components/header.js
+++ src/components/header.js
@@ -4,6 +4,7 @@ import { Link } from "gatsby"
 import styles from './styles.module.css'
 
 const Header = ({ siteTitle, siteSlogan }) => (
+
   <header
     style={{
       background: `gray`,
@@ -17,9 +18,11 @@ const Header = ({ siteTitle, siteSlogan }) => (
         padding: `1.45rem 1.0875rem`,
       }}
     >
+    <Link to='/'>
       <h1 style={{ margin: `.2rem`, fontSize:`50px` }}>
           {siteTitle}
       </h1>
+      </Link>
       <h3 style={{ margin: `0`, color: `#090909`, fontSize:`20px`, align: `center`}}>
       {siteSlogan}
       </h3>
--- src/components/project-links.js
+++ src/components/project-links.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import {Link} from 'gatsby'
+
 
 class Links extends React.Component {
   render = () => {
